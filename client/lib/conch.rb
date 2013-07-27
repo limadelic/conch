b5069b5e4bf0df50c6cfb8ff67b1709794f36ebf
mike suarez <mikemps@gmail.com>
2013-07-27 15:59:38 -0400

    refactor client code

--- client/lib/conch.rb
+++ client/lib/conch.rb
@@ -1,22 +1,7 @@
-require 'em-websocket'
+require_relative 'listener'
 
 home = 'http://www.c0nch.com'
 
 `start chrome --app=#{home}`
+Listener.start
 
-def run(socket, cmd)
-  socket.send `#{cmd}`
-rescue
-  socket.send ''
-end
-
-def listen_on(socket)
-  socket.onmessage {|cmd| run socket, cmd}
-end
-
-EM.run do
-  EM::WebSocket.run(
-    host: 'localhost',
-    port: 8888
-  ) { |socket| listen_on socket }
-end
---
+++ client/lib/listener.rb
@@ -0,0 +1,24 @@
+require 'em-websocket'
+
+module Listener
+
+  def self.run(socket, cmd)
+    socket.send `#{cmd}`
+  rescue
+    socket.send ''
+  end
+
+  def self.listen_on(socket)
+    socket.onmessage { |cmd| run socket, cmd }
+  end
+
+  def self.start
+    EM.run do
+      EM::WebSocket.run(
+        host: 'localhost',
+        port: 8888
+      ) { |socket| listen_on socket }
+    end
+  end
+
+end
